a53b1a98e4df04a3063bdbc398455a55536d0155
Ed Thomas <edthomas@eds-macbook-pro.home>
2020-11-10 18:25:10 +0000

    add dummy transactions

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const transactions = require('../data/transactions.json');
+
 router.get('/', (_, res, next) => {
   try {
-    console.log('Returning JSON');
-    res.status(200).json({ message: 'OK' });
+    res.status(200).json(transactions);
   } catch (err) {
     console.log("Error => ", err);
     return next(err);
