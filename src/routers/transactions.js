cf8c665e356f0d88bd34ea0fdfe3d02a7bcaa76c
Ed Thomas <edthomas@eds-macbook-pro.home>
2020-11-10 12:18:14 +0000

    return json

--- src/routers/transactions.js
+++ src/routers/transactions.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (_, res, next) => {
+router.get('/', (_, res, next) => {
   try {
+    console.log("Returning JSON");
     res.status(200).json({ message: 'OK' });
   } catch (err) {
+    console.log("Error => ", err);
     return next(err);
   }
 });
