5228f5ca94984319ca73ef5cc9667899d9f9e7cd
Ed Thomas <edthomas@eds-macbook-pro.home>
2020-11-10 12:10:33 +0000

    remove error handling

--- src/index.js
+++ src/index.js
@@ -6,9 +6,4 @@ const app = express();
 
 app.use('/transactions', transactionsRouter);
 
-app.use((error, _, res, next) => {
-  res.status(error.status || 500).json({ message: error.message });
-  next();
-});
-
 module.exports = app;
